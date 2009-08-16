6895258f88cb34c561a170533ce9ded52f70381e
Fernando Blat Peris <ferblape@gmail.com>
2009-08-16 22:39:17 +0200

    Don't set callbacks in test environment

--- config/initializers/filtered_collections.rb
+++ config/initializers/filtered_collections.rb
@@ -1,6 +1,8 @@
 Dir.glob(RAILS_ROOT + '/lib/collections/*.rb').each do |collection|
   require collection
-  # Sets callbacks in models
-  collection_class = collection.split('/').last.split('.').first.camelize.constantize
-  collection_class.send(:set_callbacks)
+  # Sets callbacks in models except if test environment (not so elegant)
+  unless Rails.env.test?
+    collection_class = collection.split('/').last.split('.').first.camelize.constantize
+    collection_class.send(:set_callbacks)
+  end
 end
